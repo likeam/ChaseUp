2e489aa1d917c2993f243ca913d07c7070ffab3f
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-12-28 19:18:34 +0500

    Removing Error

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -63,5 +63,35 @@ export async function registerUserController(req, res) {
 
 export async function verifyEmailController() {
   try {
-  } 
+    const { code } = request.body;
+
+    const user = await UserModel.findOne({ _id: code });
+
+    if (!user) {
+      return response.status(400).json({
+        message: "Invalid code",
+        error: true,
+        success: false,
+      });
+    }
+
+    const updateUser = await UserModel.updateOne(
+      { _id: code },
+      {
+        verify_email: true,
+      }
+    );
+
+    return response.json({
+      message: "Verify email done",
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: true,
+    });
+  }
 }
