6b7738288c843c177342d2b60bc3c73fb7498cc7
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-01-03 22:30:20 +0500

    Removing Error

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -12,7 +12,7 @@ const resend = new Resend(process.env.RESEND_API);
 const sendEmail = async ({ sendTo, subject, html }) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "ChaeUp  <noreply@amitprajapati.co.in>",
+      from: "ChaeUp  <noreply@likeam@gmail.com>",
       to: sendTo,
       subject: subject,
       html: html,
